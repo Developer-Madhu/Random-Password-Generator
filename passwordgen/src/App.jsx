e24cbe598f87241dacd0eed77ed328ff10b5adf1
Developer-Madhu <madhuatomix@gmail.com>
2025-05-05 21:09:39 +0530

    initialized tech stack

---
+++ passwordgen/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+
+  const [lengthBar, setlengthBar] = useState(10)
+  const [numbers, setlNumbers] = useState(false)
+  const [chars, setChars] = useState(false)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
