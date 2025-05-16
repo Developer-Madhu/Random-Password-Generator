c640058bb81d33a259875c511b4045096c03c329
Developer-Madhu <madhuatomix@gmail.com>
2025-05-16 06:36:18 +0530

    created range input field for password length

--- passwordgen/src/App.jsx
+++ passwordgen/src/App.jsx
@@ -12,7 +12,13 @@ const App = () => {
       <h1 className='text-white text-center my-3'>Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input className='bg-white outline-none w-full py-1 px-3' placeholder='Password' readOnly type="text" value={password} />
-        <button className='bg-blue-600 outline-none text-white py-2 shrink-0 px-3'>Copy</button>
+        <button className='bg-blue-600 outline-none cursor-pointer text-white py-2 shrink-0 px-3'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input className='cursor-pointer' onChange={(e) => setlengthBar(e.target.value)} type="range" name='' id='' value={lengthBar} min={6} max={20} />
+          <label htmlFor="length">Length: {lengthBar}</label>
+        </div>
       </div>
       </div>
   )
